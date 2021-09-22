fd4c41121df57f6bde06b804b8bb517f21fde77b
Abhay Kumar Ray <abhayray2002@gmail.com>
2021-09-22 22:07:08 +0530

    audio added

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+const audio = [];
+audio[0] = new Audio('./sounds/crash.mp3');
+audio[1] = new Audio('./sounds/kick-bass.mp3');
+audio[2] = new Audio('./sounds/snare.mp3');
+audio[3] = new Audio('./sounds/tom-1.mp3');
+audio[4] = new Audio('./sounds/tom-2.mp3');
+audio[5] = new Audio('./sounds/tom-3.mp3');
+audio[6] = new Audio('./sounds/tom-4.mp3');
+
 
 for (let index = 0; index < document.querySelectorAll("button").length; index++) {
     document.querySelectorAll("button")[index].addEventListener("click",()=>{
-        alert("I got clicked");
+        audio[index].play();
     });  
 }
+
+
+
 
 // querySelectorAll is used to get all the elements of that query.
 // We can use for loop for iterating over all the elements.
